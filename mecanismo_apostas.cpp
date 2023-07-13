cf1afe06a32c801b763f0a219c9c089bba28c84a
gabanMurilo <murilo.gaban@hotmail.com>
2023-07-13 10:23:57 -0300

    comentarios adcionados

--- mecanismo_apostas.cpp
+++ mecanismo_apostas.cpp
@@ -1,9 +1,16 @@
+// Este algoritmo escrito em C++ tem como objetivo auxiliar apostadores a 
+// decidirem se devem ou não apostar em um determinado time jogo de futebol.
+// O algoritmo utiliza como base os resultados dos ultimos 5 jogos do time
+// em questão, e com base nesses resultados, calcula a média dos ultimos jogos
+// e retorna uma chance de green ou de red para o apostador.
+
 #include "iostream"
 
 using namespace std;
 
 enum Chance { Baixa, Media, Alta };
-//Coleta de dados com base nos ultimos 5 jogos: 1=vitoria 0.5=empate; 0=derrota
+//Coleta de dados com base nos ultimos 5 jogos:
+//1=vitoria 0.5=empate; 0=derrota
 double resultadoUltimoJogo = 0; 
 double resultadoPenultimoJogo = 0;
 double resultadoAntepenultimoJogo = 0;
